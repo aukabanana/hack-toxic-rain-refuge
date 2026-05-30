08ac2a28532261e8469b5094b75e8cc54815cf6b
aukabanana <hope25501@gmail.com>
2026-05-30 15:37:03 +0700

    feat: implement get/create/delete mapzone api endpoint

--- backend/src/modules/map/routers/map.routes.ts
+++ backend/src/modules/map/routers/map.routes.ts
@@ -6,10 +6,20 @@ import {
     getMapMarkers,
 } from "../controllers/mapMarker.controller.js";
 
+import {
+    createMapZone,
+    deleteMapZone,
+    getMapZones,
+} from "../controllers/mapZone.controller.js";
+
 const mapRouter = Router();
 
 mapRouter.get("/markers", getMapMarkers);
 mapRouter.post("/markers", createMapMarker);
 mapRouter.delete("/markers/:markerId", deleteMapMarker);
+
+mapRouter.get("/zones", getMapZones);
+mapRouter.post("/zones", createMapZone);
+mapRouter.delete("/zones/:zoneId", deleteMapZone);
 
 export default mapRouter;
